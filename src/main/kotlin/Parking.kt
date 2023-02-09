a08a9271dd9475b2eca624e789d7363e960cddcc
golzibrud <v.fomina01@mail.ru>
2023-02-09 20:40:48 +0300

    Added /park_stats and /park_all_stats commands

--- src/main/kotlin/Parking.kt
+++ src/main/kotlin/Parking.kt
@@ -2,6 +2,7 @@ object Parking {
     val parkingPlaces = buildList<ParkingPlace> {
         for (i in 1..20) add(ParkingPlace(i))
     }
+    val allCarsParked = mutableSetOf<Car>()
 
     fun isFull(): Boolean {
         for (place in parkingPlaces) {
