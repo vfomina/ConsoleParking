3ff79715399f6dc9e05f95b2feb3d663492ccd73
golzibrud <v.fomina01@mail.ru>
2023-02-09 20:40:48 +0300

    Added /park_stats and /park_all_stats commands

--- src/main/kotlin/ParkingPlace.kt
+++ src/main/kotlin/ParkingPlace.kt
@@ -2,4 +2,6 @@ class ParkingPlace(val id: Int) {
     var car: Car? = null
     var isEmpty: Boolean = true
         get() = car == null
+
+    override fun toString() = "P$id : ${car ?: "Свободно"}"
 }
